f30715b8f0ce48aef675a1b15fbd236118a337a8
anass-dev-ops <anasszakaria2012@gmail.com>
2022-03-17 11:45:47 +0100

    Test Added External Module [Material] to Shared Module and used it in Private Module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [NavbarComponent, SidebarComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, SidebarComponent],
+  imports: [CommonModule, RouterModule, MatButtonModule],
+  exports: [NavbarComponent, SidebarComponent, MatButtonModule],
 })
 export class SharedModule {}
